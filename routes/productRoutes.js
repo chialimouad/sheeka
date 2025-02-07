546e5bde7fb7aa3c41c25dd15d9364f5e90abe06
Chiali Mouad <chialimouad22314@gmail.com>
2025-02-07 21:11:00 +0100

    new

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { protect, checkRole } = require('../middleware/authMiddleware');
 
-router.post('/', protect, checkRole(['admin']), productController.upload.array('images', 5), productController.addProduct);
+router.post('/add', protect, checkRole(['admin']), productController.upload.array('images', 5), productController.addProduct);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 router.put('/:id', protect, checkRole(['admin']), productController.upload.array('images', 5), productController.updateProduct);
