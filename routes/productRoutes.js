35432b31f160ebe6ef23ae4a05ca86d170f47701
Chiali Mouad <chialimouad22314@gmail.com>
2025-02-07 21:14:13 +0100

    new

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { protect, checkRole } = require('../middleware/authMiddleware');
 
-router.post('/add', protect, checkRole(['admin']), productController.upload.array('images', 5), productController.addProduct);
+router.post('/', productController.upload.array('images', 5), productController.addProduct);
 router.get('/', productController.getProducts);
-router.get('/:id', productController.getProductById);
-router.put('/:id', protect, checkRole(['admin']), productController.upload.array('images', 5), productController.updateProduct);
-router.delete('/:id', protect, checkRole(['admin']), productController.deleteProduct);
+router.put('/:id', productController.updateProduct); // ✅ Edit Product
+router.delete('/:id', productController.deleteProduct); // ✅ Delete Product
 
 module.exports = router;
