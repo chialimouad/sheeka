bb2a9395faa63db4096caea2ff9d69b9136ead92
Chiali Mouad <chialimouad22314@gmail.com>
2025-02-07 12:27:25 +0100

    new

--- server.js
+++ server.js
@@ -12,6 +12,5 @@ app.use('/uploads', express.static('uploads'));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
