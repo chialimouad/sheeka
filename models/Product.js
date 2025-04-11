e253a122b90e629a555a30bf0d862eadc730c59a
Chiali Mouad <chialimouad22314@gmail.com>
2025-04-12 01:47:12 +0200

    e

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  quantity: Number,
-  price: Number,
-  images: [String],
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  images: { type: [String], required: true },
+  variants: [{
+    colors: { type: [String], required: true },
+    sizes: { type: [String], required: true }
+  }],
 }, { timestamps: true }); // ✅ Adds createdAt & updatedAt fields automatically
 
 module.exports = mongoose.model('Product', productSchema);
