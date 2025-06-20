3001724d2d95769d8a13d3dc3b985b7a39937086
Chiali Mouad <chialimouad22314@gmail.com>
2025-06-20 15:32:57 +0200

    new

--- models/Order.js
+++ models/Order.js
@@ -1,45 +1,50 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  fullName: { 
-    type: String, 
-    required: true 
+  fullName: {
+    type: String,
+    required: true
   },
-  phoneNumber: { 
-    type: String, 
-    required: true, 
-    match: [/^(\+213|0)(5|6|7)[0-9]{8}$/, 'Invalid Algerian phone number'] 
+  phoneNumber: {
+    type: String,
+    required: true,
+    match: [/^(\+213|0)(5|6|7)[0-9]{8}$/, 'Invalid Algerian phone number']
   },
-  wilaya: { 
-    type: String, 
-    required: true 
+  wilaya: {
+    type: String,
+    required: true
   },
-  commune: { 
-    type: String, 
-    required: true 
+  commune: {
+    type: String,
+    required: true
   },
   products: [{
-    productId: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'Product', 
-      required: true 
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
     },
-    quantity: { 
-      type: Number, 
-      required: true 
+    quantity: {
+      type: Number,
+      required: true
     },
-    color: { 
-      type: String, 
-      required: true 
+    color: {
+      type: String,
+      required: true
     },
-    size: { 
-      type: String, 
-      required: true 
+    size: {
+      type: String,
+      required: true
     }
   }],
-  createdAt: { 
-    type: Date, 
-    default: Date.now 
+  status: { // Added status field
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled', 'tentative'], // Allowed values for status
+    default: 'pending' // Default status when an order is created
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
