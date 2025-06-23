579d9dc834b9143730476c16a4bb38c5e201b074
Chiali Mouad <chialimouad22314@gmail.com>
2025-06-23 16:26:08 +0200

    iji

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // ✅ Generate JWT Token
 const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, "mouadsecret", { expiresIn: '1m' });
+  return jwt.sign({ id, role }, "mouadsecret", { expiresIn: '1month' });
 };
 exports.getUsers = async (req, res) => {
   try {
