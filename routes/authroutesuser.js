e2bebff57727aca0738831d0ea470fd1d8b027bb
Chiali Mouad <chialimouad22314@gmail.com>
2025-06-27 21:03:00 +0200

    rr

--- routes/authroutesuser.js
+++ routes/authroutesuser.js
@@ -7,6 +7,6 @@ router.post('/registeruser', register);
 router.post('/loginuser', login);
 router.get('/users', getUsers); // Assuming you want this route for fetching users
 
-router.put('/users/:id/status', updateindex); // This route now correctly references the imported updateindex function
+router.put('/users', updateindex); // This route now correctly references the imported updateindex function
 
 module.exports = router;
