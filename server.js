f2fd4ebb2ab6c0095313e7222707d0cbcc3f583a
chialimouad <159821581+chialimouad@users.noreply.github.com>
2025-08-08 21:22:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ const customerRoutes = require('./routes/authroutesuser');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orders');
 // FIX: Corrected the import path to the proper routes file.
-const siteConfigRoutes = require('./routes/siteConfigRoutes'); 
+const siteConfigRoutes = require('./routes/site'); 
 const emailRoutes = require('./routes/emails');
 
 
